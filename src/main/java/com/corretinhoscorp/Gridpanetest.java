7b7875a96f138c05e284af702b8dd5972424210b
juauzitor <joao.vitor.mendes.pinto@gmail.com>
2022-05-30 11:58:27 -0300

    Testing the buttons and the funcs

--- src/main/java/com/corretinhoscorp/Gridpanetest.java
+++ src/main/java/com/corretinhoscorp/Gridpanetest.java
@@ -1,6 +1,8 @@
 package com.corretinhoscorp;
 
-import javafx.application.Application; 
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.geometry.Insets; 
 import javafx.geometry.Pos; 
 import javafx.scene.Scene; 
@@ -11,23 +13,30 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class Gridpanetest extends Application{
-    
+    private int cont;
     @Override
     public void start(Stage primaryStage) throws Exception{
     primaryStage.setTitle("GridPane Experiment");
-    
-    Button button1 = new Button("Button 1");
-    Button button2 = new Button("Button 2");
-    Button button3 = new Button("Button 3");
-    Button button4 = new Button("Button 4");
-    Button button5 = new Button("Button 5");
-    Button button6 = new Button("Button 6");
-    
+        
+    Button buttons[][] = new Button[5][5];
     GridPane gridPane = new GridPane();
 
     for (int i = 0; i < 5; i++) { // O gridPane adiciona automaticamente novas linhas e colunas então um for reolve o problema da matriz
         for (int j = 0; j < 5; j++) {
-            gridPane.add(new Button("Button" + i + j), i, j);
+            int b = cont;
+            buttons[i][j] = new Button("Button" + i +j);
+            buttons[i][j].setOnAction(new EventHandler<ActionEvent>() {
+                public void handle(ActionEvent event) {
+                    System.out.println("n = " + b);
+                }                
+            });
+            cont++;
+        }
+    }
+
+    for (int i = 0; i < 5; i++) { // O gridPane adiciona automaticamente novas linhas e colunas então um for reolve o problema da matriz
+        for (int j = 0; j < 5; j++) {
+            gridPane.add(buttons[i][j], i, j);
         }
     }    
     Scene scene = new Scene(gridPane, 240, 100);
